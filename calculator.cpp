5f4eb414269bc0f2c04447140050f1310787c9cf
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:45:44 +0200

    add a multiplication operation

--- calculator.cpp
+++ calculator.cpp
@@ -2,12 +2,18 @@
 
 int Calculator::Add (double a, double b)
 {
-
-	return a + b + 0.5;
-
+    a = a + 3;
+    b = b - 3;
+    a = b + a;
+    return a + b + 0.5;
 }
+
+
 
 int Calculator::Sub (double a, double b)
 {
+    a = a + 1;
+    b = b - 1;
     return Add (a, -b);
 }
+
